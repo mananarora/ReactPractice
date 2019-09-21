1152eeec6c838e88e255b50642300678d7382b71
Manan <manan.arora@cerner.com>
2019-09-21 23:22:21 +0530

    lec 57

--- src/Persons/Persons.js
+++ src/Persons/Persons.js
@@ -3,8 +3,10 @@ import React from 'react';
 export const Persons = (props) => {
     return (
         <div>
-            <h1>{props.name}</h1>
-            <h2>{props.address}</h2>
+            <p onClick={props.click}>
+                {props.name}
+                {props.address}
+            </p>
         </div>
     );
 };
