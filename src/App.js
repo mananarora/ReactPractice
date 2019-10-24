97e7e91a8d641c2f4a1e67ededdbce0742aebafc
Manan <manan.arora@cerner.com>
2019-10-24 05:42:52 -0500

    shouldComponentUpdate

--- src/App.js
+++ src/App.js
@@ -1,51 +1,37 @@
 import React, {Component} from 'react';
-import {Persons} from "./Persons/Persons";
 import './App.css';
 
 class App extends Component  {
-    state = {
-        Persons:[
-            {name:'Manan', Address:'Uttarakhand'},
-            {name:'Aman', Address:'Chandigarh'},
-            {name:'Vipul', Address:'India'}
-        ],
-        showNames:false
-    };
+    constructor() {
+        super();
+        this.state = {
+            value: true,
+            countOfClicks: 0
+        };
+        this.pickRandom = this.pickRandom.bind(this);
+    }
 
-    toggleHandler = () => {
+    pickRandom() {
         this.setState({
-                showNames : !this.state.showNames
+            value: Math.random() > 0.5, // randomly picks true or false
+            countOfClicks: this.state.countOfClicks + 1
         });
-    };
+    }
 
-    deleteHandler = (index) => {
-        //const persons = this.state.Persons.slice(); //Earlier approach of not using slice was modifying the state as it copies pointer.
-        const persons = [...this.state.Persons]; // This is the best practice to copy the existing state
-        persons.splice(index, 1);
-        this.setState({
-            Persons : persons
-        });
-    };
+    // comment out the below to re-render on every click
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.state.value != nextState.value;
+    }
 
     render() {
-        let persons;
-        if(this.state.showNames){
-            persons= (
-                <div>
-                    {this.state.Persons.map((person, index) => {
-                        return <Persons
-                            name={person.name}
-                            address={person.Address}
-                            click={()=>this.deleteHandler(index)}
-                        />
-                    })}
-                </div>
-            )
-        }
         return (
-            <div className="App">
-                <button onClick={this.toggleHandler}>Toggle Names</button>
-                {persons}
+            <div>
+                shouldComponentUpdate demo
+                <p><b>{this.state.value.toString()}</b></p>
+                <p>Count of clicks: <b>{this.state.countOfClicks}</b></p>
+                <button onClick={this.pickRandom}>
+                    Click to randomly select: true or false
+                </button>
             </div>
         );
     }
