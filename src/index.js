4ef62d0a195a975fb9459bc50c0ea7b9b68fd49b
Manan <manan.arora@cerner.com>
2019-12-01 15:16:22 +0530

    basic redux console count app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {createStore} from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const BUY_CAKE = 'BUY_CAKE';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const initialState = {
+    numOfCakes: 10
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numOfCakes: state.numOfCakes - action.payload
+        };
+        default: return state;
+    }
+};
+
+const store = createStore(reducer);
+
+function buyCake() {
+    return {
+        type: BUY_CAKE,
+        payload: 1
+    }
+}
+
+store.subscribe(()=> {
+    console.log('State', store.getState());
+});
+
+store.dispatch(buyCake());
+
+store.dispatch(buyCake());
+
+store.dispatch(buyCake());
