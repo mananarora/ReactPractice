403f61f12c4f590295e9c73980a0511a5d9f84e6
Manan <manan.arora@cerner.com>
2019-12-01 15:26:28 +0530

    generatord basic

--- src/index.js
+++ src/index.js
@@ -1,36 +1,25 @@
-import {createStore} from "redux";
-
-const BUY_CAKE = 'BUY_CAKE';
-
-const initialState = {
-    numOfCakes: 10
-};
-
-const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case BUY_CAKE: return {
-            ...state,
-            numOfCakes: state.numOfCakes - action.payload
-        };
-        default: return state;
-    }
-};
-
-const store = createStore(reducer);
-
-function buyCake() {
-    return {
-        type: BUY_CAKE,
-        payload: 1
-    }
+function normalFunction() {
+    return 'Hi bye'
 }
 
-store.subscribe(()=> {
-    console.log('State', store.getState());
-});
+function* generatorFunction() {
+    yield 'hi';
+    yield 'world';
+    return 'its cool'
+}
 
-store.dispatch(buyCake());
+const a = normalFunction();
+console.log(a);
 
-store.dispatch(buyCake());
+const b = generatorFunction();
+console.log(b.next());
+console.log(b.next());
+console.log(b.next());
+console.log(b.next());
 
-store.dispatch(buyCake());
+const c = generatorFunction();
+for(const val of c) {
+    console.log(val);
+}
+
+// Generator functions are used to return multiple values from single function using yield keyword.
